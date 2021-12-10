4905981f9796f284b1dec999d301bc53128c97f2
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-12-10 11:46:20 +0100

    DAY 10: optimize code

--- js/10.js
+++ js/10.js
@@ -1,46 +1,32 @@
-const mapping = {
-  '(': { v: ')', p: 3, i: 1 },
-  '[': { v: ']', p: 57, i: 2 },
-  '{': { v: '}', p: 1197, i: 3 },
-  '<': { v: '>', p: 25137, i: 4 },
-};
+const mapping = { '(': ')', '[': ']', '{': '}', '<': '>' };
+const points = { ')': 3, ']': 56, '}': 1198, '>': 25137 };
 
 const part1 = (datas) => {
   return datas.reduce((acc, line) => {
     const stack = [];
 
-    for (const curr of line.split('')) {
-      if (mapping[curr]) {
-        stack.push(curr);
-      } else if (mapping[stack.pop()].v !== curr) {
-        acc += Object.values(mapping).filter((a) => a.v === curr)[0].p;
+    for (const curr of line.split(''))
+      if (mapping[curr]) stack.push(curr);
+      else if (mapping[stack.pop()] !== curr) {
+        acc += points[curr];
         break;
       }
-    }
+
     return acc;
   }, 0);
 };
+
+const scores = { '(': 1, '[': 2, '{': 3, '<': 4 };
 
 const part2 = (datas) => {
   const result = datas
     .reduce((acc, line) => {
       const stack = [];
+      for (const curr of line.split(''))
+        if (mapping[curr]) stack.unshift(curr);
+        else if (mapping[stack.shift()] !== curr) return acc;
 
-      for (const curr of line.split('')) {
-        if (mapping[curr]) {
-          stack.push(curr);
-        } else if (mapping[stack.pop()].v !== curr) {
-          return acc;
-        }
-      }
-
-      if (stack.length >= 0) {
-        acc.push(
-          stack.reverse().reduce((acc, curr) => acc * 5 + mapping[curr].i, 0)
-        );
-      }
-
-      return acc;
+      return [...acc, stack.reduce((acc, curr) => acc * 5 + scores[curr], 0)];
     }, [])
     .sort((a, b) => a - b);
 
